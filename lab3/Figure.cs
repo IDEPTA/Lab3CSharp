ae5b6dd333371e9569315df1f3c0f1786f233538
IDEPTA <82807952+IDEPTA@users.noreply.github.com>
2023-03-08 17:44:34 +0300

    Add files via upload

---
+++ lab3/Figure.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    abstract class Figure
+    {
+        public string Name { get; set; }
+        public System.Drawing.Color Color { get; set; }
+        public System.Drawing.Point Position { get; set; }
+        public abstract double GetArea();
+        public abstract System.Drawing.Point GetCenter();
+        public abstract void Draw(Graphics gr);
+    }
+
+    class Rectangle : Figure
+    {
+        public double Width;
+        public double Height;
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X+Width/2),(int)(Position.Y+Height/2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            gr.DrawRectangle(new Pen(Color), Position.X,Position.Y,(int)Width,(int)Height);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    class Сircle : Figure
+    {
+        public double Radius { get; set; }
+        public override double GetArea()
+        {
+            return 2 * Math.PI * Math.Pow(Radius, 2);
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Radius / 2), (int)(Position.Y + Radius / 2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            Radius = Convert.ToSingle(Radius);
+            gr.DrawEllipse(new Pen(Color), Position.X, Position.Y,(int)Radius*2,(int)Radius*2);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    class Triangle : Figure
+    {
+        public double Base;
+        public double Height;
+        public override double GetArea()
+        {
+            return Base * Height / 2;
+        }
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Base / 2), (int)(Position.Y + Height / 2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            Point[] points = new Point[3];
+            points[0] = new Point(Position.X, Position.Y + (int)Height);
+            points[1] = new Point(Position.X + (int)Base, Position.Y + (int)Height);
+            points[2] = new Point(Position.X + (int)(Base / 2), Position.Y);
+            gr.DrawPolygon(new Pen(Color), points);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+
+    class Rhombus : Figure
+    {
+        public double Diagonal1 { get; set; }
+        public double Diagonal2 { get; set; }
+
+        public override double GetArea()
+        {
+            return 0.5 * Diagonal1 * Diagonal2;
+        }
+
+        public override Point GetCenter()
+        {
+            int centerX = (int)(Position.X + Diagonal2 / 2);
+            int centerY = (int)(Position.Y + Diagonal1 / 2);
+            return new Point(centerX, centerY);
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            Point[] points = new Point[4];
+            points[0] = new Point((int)(Position.X + Diagonal2 / 2), (int)Position.Y);
+            points[1] = new Point((int)(Position.X + Diagonal2), (int)(Position.Y + Diagonal1 / 2));
+            points[2] = new Point((int)(Position.X + Diagonal2 / 2), (int)(Position.Y + Diagonal1));
+            points[3] = new Point((int)Position.X, (int)(Position.Y + Diagonal1 / 2));
+            gr.DrawPolygon(new Pen(Color), points);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+
+    class Parallelogram : Figure
+    {
+        public double Base;
+        public double Height;
+        public override double GetArea()
+        {
+            return Base * Height;
+        }
+        public override Point GetCenter()
+        {
+            return new Point(Position.X + (int)(Base / 2), Position.Y + (int)(Height / 2));
+        }
+        public override void Draw(Graphics gr)
+        {
+            Point[] points = new Point[4];
+            points[0] = new Point(Position.X, Position.Y);
+            points[1] = new Point(Position.X + (int)Base, Position.Y);
+            points[2] = new Point(Position.X + (int)(Base - Height), Position.Y + (int)Height);
+            points[3] = new Point(Position.X - (int)Height, Position.Y + (int)Height);
+            gr.DrawPolygon(new Pen(Color), points);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    class Trapezium : Figure
+    {
+        public double Bottom { get; set; }
+        public double Top { get; set; }
+        public double Height { get; set; }
+
+        public override double GetArea()
+        {
+            return (Bottom + Top) * Height * 0.5;
+        }
+
+        public override Point GetCenter()
+        {
+            int x = Position.X + (int)((Bottom + Top) / 2);
+            int y = Position.Y + (int)(Height / 2);
+            return new Point(x, y);
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            Point[] points =
+            {
+                new Point((int)Position.X, (int)Position.Y),
+                new Point((int)(Position.X+Top), (int)Position.Y),
+                new Point((int)(Position.X+Bottom),(int)(Position.Y+Height)),
+                new Point((int)Position.X,(int)(Position.Y+Height))
+        };
+
+            gr.DrawPolygon(new Pen(Color), points);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    class Pentagon : Figure
+    {
+        public double Side { get; set; }
+
+        public override double GetArea()
+        {
+            return 0.25 * Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * Math.Pow(Side, 2);
+        }
+
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Side / 2), (int)(Position.Y + Side));
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            Point[] points = new Point[5];
+            double angle = 2 * Math.PI / 5;
+            double apothem = 0.5 * Side / Math.Tan(Math.PI / 5);
+            for (int i = 0; i < 5; i++)
+            {
+                points[i] = new Point(
+                (int)(Position.X + apothem * Math.Cos(i * angle - Math.PI / 2)),
+                (int)(Position.Y + apothem * Math.Sin(i * angle - Math.PI / 2))
+                );
+            }
+            gr.DrawPolygon(new Pen(Color), points);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    class Decagon : Figure
+    {
+        public double Side { get; set; }
+
+        public override double GetArea()
+        {
+            return 0.25 * Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * Math.Pow(Side, 2);
+        }
+
+        public override Point GetCenter()
+        {
+            return new Point((int)(Position.X + Side / 2), (int)(Position.Y + Side));
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            Point[] points = new Point[10];
+            double angle = 2 * Math.PI / 10;
+            double apothem = 0.5 * Side / Math.Tan(Math.PI / 10);
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(
+                (int)(Position.X + apothem * Math.Cos(i * angle - Math.PI / 2)),
+                (int)(Position.Y + apothem * Math.Sin(i * angle - Math.PI / 2))
+                );
+            }
+            gr.DrawPolygon(new Pen(Color), points);
+            gr.DrawString(GetArea().ToString(), new Font("Arial", 9), Brushes.Black, GetCenter());
+        }
+    }
+    }
---
+++ lab3/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    internal class Program
+    {
+        public static Figure[] figures =
+{
+            new Rectangle()
+            {
+                Name = "Квадрат №1",
+                Color= System.Drawing.Color.DarkRed,
+                Position = new System.Drawing.Point(30,30),
+                Width= 50,Height= 50,
+            },
+            new Rectangle()
+            {
+                Name = "Квадрат №2",
+                Color= System.Drawing.Color.Green,
+                Position = new System.Drawing.Point(60,100),
+                Width= 100,Height= 100,
+            },
+            new Rectangle()
+            {
+                Name = "Прямоугольник #1",
+                Color = System.Drawing.Color.Blue,
+                Position = new System.Drawing.Point(200,200),
+                Width= 100,Height= 50,
+            },
+            new Сircle()
+            {   
+                Name = "Круг",
+                Color = System.Drawing.Color.Blue,
+                Position = new System.Drawing.Point(200,10),
+                Radius = 25,
+            },
+            new Triangle()
+            {
+                Name = "Треугольник",
+                Color = System.Drawing.Color.Red,
+                Position = new System.Drawing.Point(300,10),
+                Height = 60, Base = 90
+            },
+            new Rhombus()
+            {
+                Name = "Ромб",
+                Color = System.Drawing.Color.Red,
+                Position = new System.Drawing.Point(400,10),
+                Diagonal1 = 85,
+                Diagonal2 = 50,
+            },
+            new Parallelogram()
+            {
+                Name = "Параллелограмм",
+                Color = System.Drawing.Color.Red,
+                Position = new System.Drawing.Point(400,100),
+                Base = 30, Height= 50
+            },
+            new Trapezium()
+            {
+                Name = "Трапеция",
+                Color = System.Drawing.Color.Green,
+                Position = new System.Drawing.Point(500,100),
+                Top= 80,Height= 50,Bottom= 100,
+            },
+            new Pentagon()
+            {
+                Name = "Пятиугольник",
+                Color = System.Drawing.Color.Red,
+                Position = new System.Drawing.Point(500,400),
+                Side= 80,
+            },
+            new Decagon()
+            {
+                Name = "Десятиугольник",
+                Color = System.Drawing.Color.Red,
+                Position = new System.Drawing.Point(500,300),
+                Side= 50,
+            }
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабараторная работа №3\nВыполнил - Прокопчук Сергей");
+
+
+            Form frm = new Form()
+            {
+                Text = "Лабараторная работа№3 - полиморфизм",
+                Size = new System.Drawing.Size(800, 600),
+                StartPosition = FormStartPosition.CenterScreen,
+            };
+           frm.Paint += Frm_Paint;
+            Application.Run(frm);
+        }
+        private static void Frm_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Figure f in figures)
+            {
+                f.Draw(e.Graphics);
+            }
+
+        }
+    }
+}
